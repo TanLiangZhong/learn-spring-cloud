dccd58f8220b14d1238d3553d6ba283716acaf74
谭良忠 <tanliangzhong@beadwallet.com>
2018-12-04 18:04:19 +0800

    add module [ml-gateway]111111

--- ml-gateway/src/main/java/com/ml/mlgateway/GatewayApplication.java
+++ ml-gateway/src/main/java/com/ml/mlgateway/GatewayApplication.java
@@ -2,7 +2,10 @@ package com.ml.mlgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 网关: gateway Server
@@ -16,5 +19,15 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
     }
 }
