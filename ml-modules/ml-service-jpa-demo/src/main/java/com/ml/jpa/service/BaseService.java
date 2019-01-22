679eb8fd200702fe381cf7b22e0436ae9ed303c2
谭良忠 <tanliangzhong@beadwallet.com>
2019-01-22 16:20:04 +0800

    add Spring Data Redis Demo Module

--- ml-modules/ml-service-jpa-demo/src/main/java/com/ml/jpa/service/BaseService.java
+++ ml-modules/ml-service-jpa-demo/src/main/java/com/ml/jpa/service/BaseService.java
@@ -1,4 +1,4 @@
-package com.ml.base.service;
+package com.ml.jpa.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
--- ml-modules/ml-service-jpa-demo/src/main/java/com/ml/jpa/service/impl/BaseServiceImpl.java
+++ ml-modules/ml-service-jpa-demo/src/main/java/com/ml/jpa/service/impl/BaseServiceImpl.java
@@ -1,6 +1,6 @@
-package com.ml.base.service.impl;
+package com.ml.jpa.service.impl;
 
-import com.ml.base.service.BaseService;
+import com.ml.jpa.service.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
