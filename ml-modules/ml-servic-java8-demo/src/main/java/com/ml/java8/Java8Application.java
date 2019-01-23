dbefaa386081f1620b596de2bc10dc09e89a82dd
谭良忠 <tanliangzhong@beadwallet.com>
2019-01-23 17:55:55 +0800

    java8 Learn

---
+++ ml-modules/ml-servic-java8-demo/src/main/java/com/ml/java8/Java8Application.java
@@ -0,0 +1,21 @@
+package com.ml.java8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * java8 Demo - Main
+ *
+ * @author liangzhong
+ * @version 1.0
+ * @date 2018-12-22
+ */
+@SpringBootApplication
+public class Java8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8Application.class, args);
+    }
+
+}
+
