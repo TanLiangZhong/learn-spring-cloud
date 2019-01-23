765de9c77a9e40dcd2c7f36d3c8758d7127bb796
谭良忠 <tanliangzhong@beadwallet.com>
2019-01-23 12:21:04 +0800

    add rabbitmq demo

---
+++ ml-modules/ml-servic-rabbitmq-demo/src/main/java/com/ml/rabbitmq/RabbitmqApplication.java
@@ -0,0 +1,21 @@
+package com.ml.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Rabbitmq Demo - Main
+ *
+ * @author liangzhong
+ * @version 1.0
+ * @date 2018-12-22
+ */
+@SpringBootApplication
+public class RabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqApplication.class, args);
+    }
+
+}
+
---
+++ ml-modules/ml-servic-rabbitmq-demo/src/main/resources/doc/SpringData-RabbitMQ-Laern.md
@@ -0,0 +1,15 @@
+### [RabbitMQ Document](http://www.rabbitmq.com/)
+
+###1. [docker-install-RabbitMQ](https://www.jianshu.com/p/14ffe0f3db94)
+
+###2. add Depending
+```xml
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-amqp</artifactId>
+</dependency>
+```
+###3. add Config
+```yaml
+
+```
